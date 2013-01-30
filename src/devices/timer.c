6e5ec13ecba9812b6903c397e7e0df9dba8d57f5
Song Han <songhan@stanford.edu>
2013-01-29 23:39:38 -0800

    fixed minor intr_yield_on_return bug in time.c

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -210,8 +210,18 @@ timer_interrupt (struct intr_frame *args UNUSED)
     while (t->wake_up_time <= ticks)
     {
       thread_unblock (t);
-      if (thread_current()->eff_priority < t->eff_priority)
-        intr_yield_on_return ();
+      if (thread_mlfqs)
+      {
+        if (thread_current()->priority < t->priority)
+          intr_yield_on_return ();
+      } 
+      else
+      {
+        if (thread_current()->eff_priority < t->eff_priority)
+          intr_yield_on_return ();
+      }
+      
+      
       list_pop_front (&alarm_list);
       if (list_empty (&alarm_list))
         break;
@@ -222,8 +232,7 @@ timer_interrupt (struct intr_frame *args UNUSED)
 
   /* advanced scheduling: update priority automatically */
   if(thread_mlfqs){
-    struct thread * cur_thread;
-    cur_thread = thread_current();
+    struct thread * cur_thread = thread_current();
     cur_thread->recent_cpu = ADD_INT(cur_thread->recent_cpu, 1);
     if(ticks % TIMER_FREQ == 0){
       calculate_load_avg();
