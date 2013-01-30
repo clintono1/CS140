f2e38a01f57114507668124c1ef53786bdc9441e
Jinchao Ye <yjc111@gmail.com>
2013-01-29 23:21:14 -0800

    initialize load_avg

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -106,6 +106,7 @@ thread_init (void)
   init_thread (initial_thread, "main", PRI_DEFAULT);
   initial_thread->status = THREAD_RUNNING;
   initial_thread->tid = allocate_tid ();
+  load_avg = 0;
 }
 
 /* Starts preemptive thread scheduling by enabling interrupts.
