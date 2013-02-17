177b38cfd15f5ebcabae0659f8a761972a0e89ca
Jinchao Ye <yjc111@gmail.com>
2013-02-16 17:19:50 -0800

    incomplete version of vm_allocate_frame

---
+++ src/vm/frame.c
@@ -0,0 +1,20 @@
+#include "vm/frame.h"
+#include "threads/palloc.h"
+
+void *
+vm_allocate_frame(enum palloc_flags){
+	void *frame = NULL;
+	if(flags & PAL_USER){
+		if(flags & PAL_ZERO){
+			frame = palloc_get_page(PAL_USER | PAL_ZERO);
+		}
+		else{
+			frame = palloc_get_page(PAL_USER);
+		}
+	}
+	if(frame)
+		/* add to frame list */
+	else
+		/* eviction */
+	return frame;
+}
---
+++ src/vm/frame.h
@@ -0,0 +1,6 @@
+#ifndef VM_FRAME_H
+#define VM_FRAME_H
+
+void *vm_get_frame(enum palloc_flags);
+
+#endif /* vm/frame.h */
