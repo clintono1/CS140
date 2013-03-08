557405940b5da0a5ed98117a28357276830fc438
Bo Wang <bowang@stanford.edu>
2013-03-07 22:18:08 -0800

    Added PRINTF macro. To enable PRINTF, append pdebug=true after make, e.g. make pdebug=true

--- src/lib/debug.h
+++ src/lib/debug.h
@@ -38,9 +38,8 @@ void debug_backtrace_all (void);
 #define NOT_REACHED() for (;;)
 #endif /* lib/debug.h */
 
-// #define PDEBUG
 #ifdef PDEBUG
-#define PRINTF(...) printf(__VA_ARGS__)
+#define PRINTF(...) printf (__VA_ARGS__)
 #else
 #define PRINTF(...)
 #endif
